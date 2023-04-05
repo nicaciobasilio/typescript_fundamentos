6c6e24a2f137c74fdcff1567943027a53177d4db
Nicacio Basilio <nicacioufrn@gmail.com>
2023-04-05 20:43:30 -0300

    adicionando tipos básicos

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,24 @@
+let estaChovendo: boolean = false;
+
+estaChovendo = true;
+
+let idade: number = 21;
+
+let altura: number = 1.75;
+
+const nacionalidade: string = 'brasileiro';
+
+const colegas: string[] = ['lucas', 'fernanda'];
+const tecnologias: Array<string> = ['HTML', 'CSS', 'JS'];
+const notas: ReadonlyArray<number> = [7, 6, 8];
+
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['Nicacio', true, 27];
+
+let idadeDaAna: number | string = 25;
+idadeDaAna = '25 anos';
+
+let dadosDaApi: any;
+dadosDaApi = true;
+dadosDaApi = true;
+dadosDaApi = [1,2,3];
+dadosDaApi = 'string';
