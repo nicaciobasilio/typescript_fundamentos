d195e3fc417d043f6f1ae6aacc4ec18cb784b141
Nicacio Basilio <nicacioufrn@gmail.com>
2023-04-06 00:02:10 -0300

    salvamento

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+"use strict";
+const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByClassName('form');
+const botao = document.getElementById('btn-enviar');
+botao === null || botao === void 0 ? void 0 : botao.addEventListener('click', (e) => {
+});
---
+++ src/dom.ts
@@ -0,0 +1,7 @@
+const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByClassName('form');
+const botao = document.getElementById('btn-enviar');
+
+botao?.addEventListener('click', (e: MouseEvent) => {
+    
+})
